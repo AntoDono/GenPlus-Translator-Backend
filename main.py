6281136247ff0fe878a5ef7b7f6bc0acda4eec45
AntoDono <anthony20151128@gmail.com>
2024-09-21 15:41:45 -0400

    added vocab

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def translate():
     except:
         return 500
     
-    return dict(result=translation, words=wowords_with_definitionrds)    
+    return dict(result=translation, words=words_with_definition)    
     
 if __name__ == "__main__":
     app.run(debug=False)
